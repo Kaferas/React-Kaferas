744063531dbff2fa1b1b8ca35208de0d74e55c93
Kaferas <chrisirak95@gmail.com>
2022-02-12 06:12:06 +0200

    Changing text

--- src/components/Heroku.js
+++ src/components/Heroku.js
@@ -3,12 +3,15 @@ function Heroku()
 {
     return (<Benchu>
         <h2 >Good Morning {"Kaferas".toUpperCase()}</h2>
+        <h4>React Has Become a popular FrontEnd Framework So Go On </h4>
     </Benchu>)
 }
 
 export default Heroku
 const Benchu=styled.div`
-    background-color:red;
-    border-radius:20px
-    width:100vw;
+    background-color:rgba(34,232,165,.7);
+    border-radius:4%;
+    align-text:center;
+    margin-top:20px;
+    width:40vw;
 `
